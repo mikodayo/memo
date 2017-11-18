a905dc5b9541404972c7b485978b4ef945ccdb14
mikodayo <t1799916@ied3.(none)>
2017-11-18 16:09:54 +0900

    curlにオプションをいくつか追加

--- url_checker.sh
+++ url_checker.sh
@@ -7,7 +7,11 @@ if  [ -e "$1" ] && [ -e "$1" ]; then
         cat "$1" | while read line
         do
 	    echo "aaa" . $line
-            result=`curl -LI $line -o /dev/null -w '%{http_code}\n' -s`
+            # --location: リダイレクトに対応
+            # --get: HTTP GETリクエストで送信する
+            # --compressed: 圧縮に対応
+            # --user-agent: ユーザエージェントを指定
+            result=`curl --location --get --compressed --user-agent 'Mozilla/4.0 (compatible; MSIE 6.0; Windows NT 5.1; SV1; .NET CLR 1.0.3705; .NET CLR 1.1.4322)' $line -o /dev/null -w '%{http_code}\n' -s`
             sleep 1
             echo $line, $result
         done
