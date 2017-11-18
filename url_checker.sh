fc08ceebe0107db2c7f490961eff6e41e07ddd52
t1799916 <t1799916@ied3.(none)>
2017-11-18 14:08:08 +0900

    引数チェック

--- url_checker.sh
+++ url_checker.sh
@@ -1,15 +1,19 @@
 # 引数に渡したファイル(１行ずつURLが書いてある想定)をチェックしステータスコードを吐くよ
 #!/bin/bash
 
-if [ -e $1 ]; then
 
-        cat $1 | while read line
+if  [ -e "$1" ] && [ -e "$1" ]; then
+
+        cat "$1" | while read line
         do
+	    echo "aaa" . $line
             result=`curl -LI $line -o /dev/null -w '%{http_code}\n' -s`
             sleep 1
             echo $line, $result
         done
 else
+  	echo "usage ./url_checker.sh fine_full_path"
+  	echo "引数にファイル名を指定してください。"
         echo "ファイルなし"
 
 fi
